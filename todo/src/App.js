92ccc9886cb243d7c50ff612ac61d24034ff1cc9
Shaurya <shauryamishra2279@gmail.com>
2020-09-30 16:58:59 +0530

    React setup

---
+++ todo/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Tasks for the Day</p>
+
+				<p>Done & Dusted</p>
+			</header>
+		</div>
+	);
+};
+
+export default App;
